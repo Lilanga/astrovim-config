792a4255db86c0c3201da4dcf216a50c42db7f7d
Lilanga Gamage <lilanga@live.com>
2024-02-18 01:40:00 +1100

    added language servers, linters and neoscroll

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -5,11 +5,46 @@ return {
     "williamboman/mason-lspconfig.nvim",
     -- overrides `require("mason-lspconfig").setup(...)`
     opts = function(_, opts)
+      opts.automatic_installation = true
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
         -- `community.lua` ensures language servers via their language packs
         "marksman", -- Markdown structure (also in markdown pack)
-        -- "yamlls",
+        "yamlls",
+        "yamlfmt",
+        "yamllint",
+        "json-lsp",
+        "jsonlint",
+        "prettier",
+        "eslint_d",
+        "clojure_lsp",
+        "rust_analyzer",
+        "go-debug-adapter",
+        "gopls",
+        "gofumpt",
+        "goimports",
+        "golines",
+        "typescript-language-server",
+        "yaml-language-server",
+        "editorconfig-checker",
+        "gitleaks",
+        "omnisharp",
+        "csharpier",
+        "csharp-language-server",
+        "eslint_lsp",
+        "haskell-language-server",
+        "haskell-debug-adapter",
+        "htmlbeautifier",
+        "htmx-lsp",
+        "java-language-server",
+        "java-debug-adapter",
+        "protolint",
+        "synk",
+        "node-debug2-adaptor",
+        "netcoredbg",
+        "delve",
+        "docker-language-server",
+        "hadolint",
       })
     end,
   },
