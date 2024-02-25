50b84da1d7452810f9ed86cc32ca0125cd39e9a3
Lilanga Gamage <lilanga@live.com>
2024-02-25 12:14:31 +1100

    code completion configs

--- plugins/cmp.lua
+++ plugins/cmp.lua
@@ -1,21 +1,3 @@
-local has_words_before = function()
-        if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_text(0, line-1, 0, line-1, col, {})[1]:match("^%s*$") == nil
-    end
-    local cmp = require("cmp")
-    cmp.setup({
-            mapping = {
-            ["<Tab>"] = vim.schedule_wrap(function(fallback)
-                if cmp.visible() and has_words_before() then
-                cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-            else
-                fallback()
-            end
-            end),
-            },
-        })
-
 return {
     "hrsh7th/nvim-cmp",
     opts = {
@@ -28,12 +10,12 @@ return {
         ghost_text = true,
       },
       sources = {
-        { name = "nvim_lsp", priority = 1000 },
-        { name = "codeium",  priority = 750 },
-        { name = "copilot",  priority = 750 },
-        { name = "luasnip",  priority = 700 },
-        { name = "path",     priority = 650 },
-        { name = "buffer",   priority = 400 },
+        { name = "nvim_lsp", priority = 1000, group_index = 2 },
+        { name = "codeium",  priority = 750, group_index = 2 },
+        { name = "copilot",  priority = 750, group_index = 2 },
+        { name = "luasnip",  priority = 700, group_index = 2 },
+        { name = "path",     priority = 650, group_index = 2 },
+        { name = "buffer",   priority = 400, group_index = 2 },
       }
     }
   }
